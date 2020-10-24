4c7b30e7f6f6bcef8aa1bb3d6b96c855d5f16643
Vitaly Gaiduk <termvitx@gmail.com>
2020-10-25 02:59:50 +0300

    Task1: Add convert positive number to binary

--- Task1/Task1/main.cpp
+++ Task1/Task1/main.cpp
@@ -4,7 +4,44 @@
 
 #include <iostream>
 
+#define STR_SIZE (sizeof(int)*8 + 1) // for radix=2 (also add 1 byte for '\0')
+
+char* itob(int val, char* str, int size)
+{
+	short sign = 0;
+	char* p = str;
+
+	// move to end
+	p += size - 1;
+
+	if (val < 0) {
+		sign = 1;
+		val = -val;
+	}
+
+	do
+	{
+		*p = val % 2 + '0';
+		p--;
+		val /= 2;
+	} while (val);
+
+	return str;
+}
+
 int main()
 {
+	int n;
+	char str[STR_SIZE] = { 0 };
+
+	memset(str, '0', (STR_SIZE - 1) * sizeof(char));
+
+	std::cout << "Input number: ";
+	std::cin >> n;
+
+	itob(n, str, STR_SIZE - 1);
+
+	std::cout << "Binary format: " << str << std::endl;
+
 	return 0;
 }
