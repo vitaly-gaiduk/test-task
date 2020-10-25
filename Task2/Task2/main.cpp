4e4d7fb4b2dac72b4005376fc2bf351edbc99753
Vitaly Gaiduk <termvitx@gmail.com>
2020-10-25 03:06:06 +0300

    Initial commit of Task2

---
+++ Task2/Task2/main.cpp
@@ -0,0 +1,18 @@
+/* 2. Ќапишите функцию, удал€ющую последовательно дублирующиес€ символы в строке:
+* 
+void RemoveDups(char* str);
+
+// пример использовани€
+char data[] = УAAA BBB AAAФ;
+RemoveDups(data);
+printf(У% s\nФ, data); // УA B AФ
+
+*/
+
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
