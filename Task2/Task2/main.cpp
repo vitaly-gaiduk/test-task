ac51685e02c168004350a9ea9c653ec088b1dc81
Vitaly Gaiduk <termvitx@gmail.com>
2020-10-25 18:47:58 +0300

    Task2: Remove duplicate symbols from string

--- Task2/Task2/main.cpp
+++ Task2/Task2/main.cpp
@@ -1,5 +1,5 @@
 /* 2. Ќапишите функцию, удал€ющую последовательно дублирующиес€ символы в строке:
-* 
+*
 void RemoveDups(char* str);
 
 // пример использовани€
@@ -9,10 +9,39 @@ printf(У% s\nФ, data); // УA B AФ
 
 */
 
+#include <cstdio>
+#include <cstring>
 
-#include <iostream>
+#define DATA_LEN 256
+
+void RemoveDups(char* str)
+{
+	char* pStr = str;
+	char res[DATA_LEN] = { 0 };
+	char* pRes = res;
+
+	while (*pStr != '\0') {
+		*pRes = *pStr;
+		while (*pStr == *(pStr + 1)) {
+			pStr++;
+		}
+		pStr++;
+		pRes++;
+	}
+
+	memcpy(str, res, sizeof(char) * DATA_LEN);
+}
 
 int main()
 {
+	char data[DATA_LEN] = { 0 };
+
+	printf("Input string: ");
+	fgets(data, sizeof(char) * DATA_LEN, stdin);
+
+	RemoveDups(data);
+
+	printf("Parsed: %s\n", data);
+
 	return 0;
 }
