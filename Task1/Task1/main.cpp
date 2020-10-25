38cecdb8df42316ba2add5a76a13dab0cb388e65
Vitaly Gaiduk <termvitx@gmail.com>
2020-10-25 03:01:41 +0300

    Task1: Add handling of the sign number

--- Task1/Task1/main.cpp
+++ Task1/Task1/main.cpp
@@ -6,6 +6,41 @@
 
 #define STR_SIZE (sizeof(int)*8 + 1) // for radix=2 (also add 1 byte for '\0')
 
+char* compl_code(char* str, int size)
+{
+	char* p = str;
+	short carry = 1;
+
+	// To create complement code:
+	// 1. Invert bits
+	// 2. Add 1 to number
+
+	while (*p != '\0') { // invert bits
+		if (*p == '0')
+			*p = '1';
+		else
+			*p = '0';
+
+		p++;
+	}
+
+	// move to end
+	p = str + size - 1;
+
+	while (carry) { // add 1 to number
+		if (*p == '0') {
+			*p = '1';
+			carry = 0;
+		}
+		else {
+			*p = '0';
+		}
+		p--;
+	}
+
+	return str;
+}
+
 char* itob(int val, char* str, int size)
 {
 	short sign = 0;
@@ -25,6 +60,9 @@ char* itob(int val, char* str, int size)
 		p--;
 		val /= 2;
 	} while (val);
+
+	if (sign)
+		compl_code(str, size);
 
 	return str;
 }
